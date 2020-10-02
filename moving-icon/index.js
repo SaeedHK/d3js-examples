01ba08d525fdaca376d43495a426e33507458beb
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-10-02 09:58:14 +0200

    feat: icon animates

--- moving-icon/index.js
+++ moving-icon/index.js
@@ -1,15 +1,25 @@
-var icon = d3.select("#icon");
+var icon = d3.select("#icon").attr("src", "rocket.svg").attr("width", "100px");
 
-var timeSteps = 300;
-var duration = 50;
-var translateXRange = 10;
-var translateYRange = 10;
-for (var i = 0; i < timeSteps; i++) {
+const timeSteps = 100;
+const duration = 20;
+const transXRange = 5;
+const transYRange = 5;
+const scaleRange = 0;
+
+const timeAdapt = function (t, range) {
+  var timeEffect = 1 - t / timeSteps;
+  return range * timeEffect;
+};
+
+for (var t = 0; t < timeSteps; t++) {
   //var angle = d3.randomUniform(-45, 45)();
   var angle = 0;
-  var scale = d3.randomUniform(0.8, 1.2)();
-  var translateX = d3.randomUniform(-translateXRange, translateXRange)();
-  var translateY = d3.randomUniform(-translateYRange, translateYRange)();
+  var adaptedScaleRange = timeAdapt(t, scaleRange);
+  var adaptedTransXRange = timeAdapt(t, transXRange);
+  var adaptedTransYRange = timeAdapt(t, transYRange);
+  var scale = d3.randomUniform(1 - adaptedScaleRange, 1 + adaptedScaleRange)();
+  var translateX = d3.randomUniform(-adaptedTransXRange, adaptedTransXRange)();
+  var translateY = d3.randomUniform(-adaptedTransYRange, adaptedTransYRange)();
   icon = icon
     .transition()
     .style(
